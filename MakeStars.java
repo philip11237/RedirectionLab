9945ada440c653cd387f54ce5a661a3926d33f34
philip von mueffling <philip.vm3@gmail.com>
2021-01-04 22:37:50 -0500

    makestars

---
+++ MakeStars.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class MakeStars{
+  public static void main (String args[]){
+    Scanner scan=new Scanner(System.in);
+    while (scan.hasNextLine()){
+      String line=can.nextLine();
+      for int i=0; i<line.length();i++;{
+        if (line.charAt(i)!=' '){
+          System.out.print('*' );
+        }
+        else{
+          System.out.print(' ');
+        }
+      }System.out.println();
+    }
+  }
+}
