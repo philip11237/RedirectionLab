7524378b4480117b78d49450d3f5e20de5f639e6
philip von mueffling <philip.vm3@gmail.com>
2021-01-04 23:30:37 -0500

    commit

--- MakeStars.java
+++ MakeStars.java
@@ -3,7 +3,7 @@ public class MakeStars{
   public static void main (String args[]){
     Scanner scan=new Scanner(System.in);
     while (scan.hasNextLine()){
-      String line=can.nextLine();
+      String line=scan.nextLine();
       for (int i=0; i<line.length();i++){
         if (line.charAt(i)!=' '){
           System.out.print('*' );
--- PigLatin.java
+++ PigLatin.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class PigLatin{
   public static String pigLatinSimple(String s){
-    if (s.charAt(0)='a'||s.charAt(0)='e'||s.charAt(0)='i'||s.charAt(0)='o'||s.charAt(0)='u'){
+    if (s.charAt(0)=='a'||s.charAt(0)=='e'||s.charAt(0)=='i'||s.charAt(0)=='o'||s.charAt(0)=='u'){
       return s+"hay";
     }
 
@@ -13,11 +13,11 @@ public class PigLatin{
     String list[]= {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
 
     for (int i=0;i<list.length;i++){
-      if(s.substring(0,2).equals(list[i])){
+      if ((s.length()>1) && (s.substring(0,2).equals(list[i]))){
         return s.substring(2)+list[i]+"ay";
       }
     }
-    if (s.charAt(0)='a'||s.charAt(0)='e'||s.charAt(0)='i'||s.charAt(0)='o'||s.charAt(0)='u'){
+    if (s.charAt(0)=='a'||s.charAt(0)=='e'||s.charAt(0)=='i'||s.charAt(0)=='o'||s.charAt(0)=='u'){
       return s+"hay";
     }
 
@@ -30,11 +30,12 @@ public class PigLatin{
 
   }
   public static String pigLatinBest(String s){
+    s=s.toLowerCase();
     if (!Character.isAlphabetic(s.charAt(0))){
       return s;
     }
     if (!Character.isAlphabetic(s.charAt(s.length()-1)))
-    return pigLatin(s.substring(0,s.length()-1)+s.charAt(s.length()-1);
+    return pigLatin(s.substring(0,s.length()-1))+s.charAt(s.length()-1);
     else{
       return pigLatin(s);
     }
@@ -42,12 +43,12 @@ public class PigLatin{
   public static void main (String args[]){
     Scanner scan=new Scanner(System.in);
     while (scan.hasNextLine()){
-      String line=can.nextLine();
+      String line=scan.nextLine();
       String words[]=line.split(" ");
-      for (int i=0; i<line.length();i++){
-        System.out.print(words[i]+' ');
+      for (int i=0; i<words.length;i++){
+        System.out.print(pigLatinBest(words[i])+' ');
 
-      }
+      }System.out.println();
     }
 
   }
