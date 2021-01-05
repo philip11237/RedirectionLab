a26acc51f085ba66e5e8f3e2f698740f461f625e
philip von mueffling <philip.vm3@gmail.com>
2021-01-04 22:59:29 -0500

    PL2

--- PigLatin.java
+++ PigLatin.java
@@ -8,6 +8,25 @@ public class PigLatin{
     else{
       return s.substring(1)+s.charAt(0)+"ay";
     }
+  }
+  public static String pigLatin(String s){
+    String list[]= {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+
+    for (int i=0;i<list.length;i++){
+    if(s.substring(0,2).equals(list[i])){
+      return s.substring(2)+list[i]+"ay";
+    }
+  }
+    if (s.charAt(0)='a'||s.charAt(0)='e'||s.charAt(0)='i'||s.charAt(0)='o'||s.charAt(0)='u'){
+      return s+"hay";
+    }
+
+    else{
+      return s.substring(1)+s.charAt(0)+"ay";
+    }
+
+
+
 
   }
 }
